687f5c5bec7b9303936c7cd77ff110821775c252
Michael Reifer <Mjreifer1@gmail.com>
2025-03-26 11:26:32 -0400

    Basic web server in python

---
+++ MyWebServer.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import threading
+
+serverPort = 8888
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def respond(connectionSocket, address):
+    request = connectionSocket.recv(1024).decode()
+
+    ###### Fill in Start ######
+    lines = request.split("\r\n")
+    for line in lines:
+        print(line)
+
+    response = 'HTTP/1.1 200 OK\r\n'
+    print(response) 
+
+    connectionSocket.send(response.encode())
+    connectionSocket.close()
+    ###### Fill in End ######
+
+    return
+
+# start the web server
+try:
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind(('', serverPort))
+    print("The server is ready to receive on port {port}.\n".format(port=serverPort))
+except Exception as e:
+    print("An error occurred on port {port}\n".format(port=serverPort))
+    serverSocket.close()
+    sys.exit(1)
+
+serverSocket.listen(1)
+
+# handle requests
+while True:
+    (connectionSocket, address) = serverSocket.accept()
+    print("Received connection from {addr}\n".format(addr=address))
+    threading.Thread(target=respond, args=(connectionSocket, address)).start()
