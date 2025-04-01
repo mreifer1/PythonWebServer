ec71e30b2a9db33442a17aa7804a0fdb46293387
Michael Reifer <Mjreifer1@gmail.com>
2025-04-01 12:37:46 -0400

    Dealing with methods other then head and get reqs

--- MyWebServer.py
+++ MyWebServer.py
@@ -1,27 +1,46 @@
 import socket
 import sys
 import threading
+from datetime import datetime, timezone
 
 serverPort = 8888
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+acceptedRequests = ["GET", "HEAD"]
+
+def get_current_date():
+    return datetime.now(timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')
 
 def respond(connectionSocket, address):
     request = connectionSocket.recv(1024).decode()
 
-    ###### Fill in Start ######
     lines = request.split("\r\n")
     request_line = lines[0]
-    method = request_line.split()[0]
+    method, file_path, http_version = request_line.split()    
 
     for line in lines:
         print(line)
 
+    
+    if method not in acceptedRequests: # If the request is not a head or get req
+
+        response = 'HTTP/1.1 501 Not Implemeneted\r\n'
+        response += 'Date: ' +get_current_date() + '\r\n'
+        response += 'Connection: keep-alive\r\n'
+        response += 'Server: MyWebServer\r\n'
+        response += "Content-Length: 0\r\n\r\n"
+
+        print(f"Sending Response:\n{response}")
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return 
+    
     response = 'HTTP/1.1 200 OK\r\n'
-    print(response) 
+    response += 'Date: ' +get_current_date() + '\r\n\r\n'
+
+    print(f"Sending Response to client:\n\n{response}")
 
     connectionSocket.send(response.encode())
     connectionSocket.close()
-    ###### Fill in End ######
 
     return
 
