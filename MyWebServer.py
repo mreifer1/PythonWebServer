822b8aff97e852b162e41281f1d32aa418fbc4a7
Michael Reifer <Mjreifer1@gmail.com>
2025-04-01 16:52:22 -0400

    added alot to the server

--- MyWebServer.py
+++ MyWebServer.py
@@ -2,6 +2,8 @@ import socket
 import sys
 import threading
 from datetime import datetime, timezone
+import os
+import time
 
 serverPort = 8888
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,36 +15,66 @@ def get_current_date():
 def respond(connectionSocket, address):
     request = connectionSocket.recv(1024).decode()
 
+    if_modified_since = ''
+    flag = False
+    file_path = 'index.html'
+    modification_time = os.path.getmtime(file_path)
+    last_modified = time.gmtime(modification_time)
+    formatted_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT", last_modified)
+
     lines = request.split("\r\n")
     request_line = lines[0]
-    method, file_path, http_version = request_line.split()    
+    method, Req_file_path, http_version = request_line.split()
 
     for line in lines:
-        print(line)
+        if line.startswith("If-Modified-Since"):
+            flag = True
+            if_modified_since = line.split(":", 1)[1].strip()
+            break
 
+    print("Request Method: ", method)
+    print("Requested File: ", Req_file_path)
+    
     
     if method not in acceptedRequests: # If the request is not a head or get req
 
         response = 'HTTP/1.1 501 Not Implemeneted\r\n'
         response += 'Date: ' +get_current_date() + '\r\n'
-        response += 'Connection: keep-alive\r\n'
         response += 'Server: MyWebServer\r\n'
-        response += "Content-Length: 0\r\n\r\n"
+        response += "Content-Length: 112\r\n\r\n" # this 112 value was calculated from len(response.encode()) and is accurate
 
         print(f"Sending Response:\n{response}")
         connectionSocket.send(response.encode())
         connectionSocket.close()
         return 
-    
-    response = 'HTTP/1.1 200 OK\r\n'
-    response += 'Date: ' +get_current_date() + '\r\n\r\n'
+    elif Req_file_path not in ['/', '/index.html']:
 
-    print(f"Sending Response to client:\n\n{response}")
+        response = 'HTTP/1.1 404 Not Found\r\n'
+        response += 'Date: ' +get_current_date() + '\r\n'
+        response += 'Server: MyWebServer\r\n'
+        response += "Content-Length: 105\r\n\r\n" # this 105 value was calculated from len(response.encode()) and is accurate
 
-    connectionSocket.send(response.encode())
-    connectionSocket.close()
+        print(f"Sending Response:\n{response}")
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return
+    elif method == "HEAD": # head req
+        
+        response = 'HTTP/1.1 200 OK\r\n'
+        response += 'Date: ' +get_current_date() + '\r\n'
+        response += 'Connection: keep-alive\r\n'
+        response += 'Server: MyWebServer\r\n'
+        response += f'Last-Modified: {formatted_time}\r\n'
+        response += "Content-Length: 168\r\n\r\n" # this 168 value was calculated from len(response.encode()) and is accurate
 
-    return
+        print(f"Sending Response:\n{response}")
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        return
+    #elif method == "GET": # get req
+        
+
+    #return
 
 # start the web server
 try:
